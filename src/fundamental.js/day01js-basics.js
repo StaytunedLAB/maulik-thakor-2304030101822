6946d94e52154bbe8779b2ee7f10cb6317cbd11f
maulik <tmaulik976@gamil.com>
2025-12-01 14:41:51 +0530

    code folder

---
+++ src/fundamental.js/day01js-basics.js
@@ -0,0 +1,4 @@
+console.log ("Hello, world!");
+console.log ("Day 01 - javascrript basics");
+/* basic part-1  java script learning to ./ this Example code to learn for "Hello world"*/ 
+console.log("hii")
