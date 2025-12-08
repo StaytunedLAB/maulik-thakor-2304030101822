de43e95a2a925f2e5a06120772c4a5e3db124c68
maulik <tmaulik976@gamil.com>
2025-12-08 13:38:09 +0530

    New Practice folder

--- src/practice/Task1.js
+++ src/practice/Task1.js
@@ -29,3 +29,22 @@ for (let i = 2; i < n; i++) {
     b = c;
 }
 
+// Q3. Grade Calculator using Switch Case*
+
+javascript
+let marks = parseInt(prompt("Enter marks: "));
+let grade;
+
+switch (true) {
+    case marks >= 90: grade = "A"; break;
+    case marks >= 80: grade = "B"; break;
+    case marks >= 70: grade = "C"; break;
+    case marks >= 60: grade = "D"; break;
+    case marks >= 50: grade = "E"; break;
+    default: grade = "F";
+}
+
+console.log("Your Grade is: " + grade);
+
+
+
